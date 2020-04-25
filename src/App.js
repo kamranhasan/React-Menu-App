a8a62b77a4ee85c3eaaebe8fb49d010bd2dcaead
Kamran Hasan <kamranhasann@gmail.com>
2020-04-25 22:01:50 +0500

    menu component using props

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from "reactstrap";
 import './App.css';
 import Menu from './Components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      dishes:DISHES,
+    };
+  }
+
   render(){
   return (
     <div>
@@ -13,7 +22,7 @@ class App extends Component {
          <NavbarBrand href='/'>Food Fusion App</NavbarBrand> 
         </div>  
       </Navbar> 
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
   );
 }
