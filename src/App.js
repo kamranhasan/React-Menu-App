fa14e0e4d2ec8b54446ed433e147693bf6374e0c
Kamran Hasan <kamranhasann@gmail.com>
2020-04-28 03:02:19 +0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Main from "./Components/MainComponent";
 import './App.css';
-
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
   
   render(){
   return (
+    <BrowserRouter>
     <div>
       <Main />
     </div>
+    </BrowserRouter>
   );
 }
 }
