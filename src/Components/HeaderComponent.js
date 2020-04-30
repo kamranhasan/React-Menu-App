843803b8173fc226effee97c77f367c5f9fb1165
Kamran Hasan <kamranhasann@gmail.com>
2020-05-01 02:57:21 +0500

    Uncontrolled form with Login functionality added

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand,Nav,NavbarToggler,Collapse,NavItem, Jumbotron } from "reactstrap";
+import { Navbar, NavbarBrand,Nav,NavbarToggler,Button,Collapse,Form,FormGroup,Label,Input,NavItem, Jumbotron,Modal,ModalBody,ModalHeader } from "reactstrap";
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component{
@@ -7,8 +7,11 @@ class Header extends Component{
         super(props);
         this.state = {
             isNavOpen: false,
+            isModalOpen: false,
         }
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
     toggleNav(){
         this.setState(
@@ -17,7 +20,18 @@ class Header extends Component{
         }
         );
     }
-
+    toggleModal(){
+        this.setState(
+            {
+                isModalOpen: !this.state.isModalOpen,
+            }
+            );
+    }
+    handleLogin(event){
+        this.toggleModal();
+        alert('Username: '+ this.username.value + 'Password: '+ this.password.value + 'Remember: '+ this.remember.value);
+        event.preventDefault();
+    }
     render(){
         return(
             <>
@@ -28,7 +42,7 @@ class Header extends Component{
                 <img src='assets/images/logo.png' width='41' height='30' alt="Food Fusion"/>     
             </NavbarBrand> 
             <Collapse isOpen={this.state.isNavOpen} navbar>
-            <Nav navbar>
+            <Nav className='ml-auto' navbar>
                 <NavItem>
                     <NavLink className='nav-link' to="/home">
                         <span className='fa fa-home fa-lg'></span>
@@ -54,6 +68,13 @@ class Header extends Component{
                     </NavLink>
                 </NavItem>
             </Nav>
+            <Nav className='ml-auto' navbar>
+                <NavItem>
+                    <Button outline onClick={this.toggleModal}>
+                        <span className='fa fa-sign-in fa-lg'>Login</span>
+                    </Button>
+                </NavItem>
+            </Nav>
             </Collapse>
             </div>  
         </Navbar> 
@@ -67,6 +88,28 @@ class Header extends Component{
                 </div>
             </div>
         </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader>Login</ModalHeader>
+            <ModalBody>
+                <Form onSubmit={this.handleLogin}>
+                    <FormGroup>
+                        <Label htmlFor='username'>Username</Label>
+                        <Input type='text' id='username' name='username' innerRef={(input)=> this.username=input}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='password'>Password</Label>
+                        <Input type='password' id='password' name='password' innerRef={(input)=> this.password=input}/>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type='checkbox' name='remember' innerRef={(input)=> this.remember=input}/>
+                            Remember me
+                        </Label>
+                    </FormGroup>
+                    <Button type='submit' value='submit' color='primary'>Login</Button>
+                </Form>
+            </ModalBody>
+        </Modal>
             </>
         );
     }
